37d2f8af60970779dd9d7cc3dff836b89d648db4
georgeberry <geb97@cornell.edu>
2014-01-20 20:19:19 -0500

    removed

--- blaunet-package/R/active.R
+++
@@ -1,4 +0,0 @@
-active <-
-function(blauObj){
-  sprintf('These elements are active: %s. Access with object$element.', paste(names(blauObj), collapse = ", "))
-}
--- blaunet-package/R/export.dyadic.R
+++
@@ -1,9 +0,0 @@
-export.dyadic <-
-function(blauObj){
-  if (is.null(blauObj$dyadic)){
-    print("Nothing to export.")
-  }
-  else{
-    return(blauObj$dyadic)
-  }
-}
